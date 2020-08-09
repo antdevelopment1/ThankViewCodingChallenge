c4538fae1c775c3c84b19349ef02e097e1f1e2ff
April Copes <antdevelopment1@gmail.com>
2020-08-09 15:43:57 -0400

    adds computed methods

--- scripts.js
+++ scripts.js
@@ -1,10 +1,33 @@
 new Vue({
     // el is short for the word element
     el: "#app",
+    data: {
+        inputText: ''
+    },
+    computed: {
+        identicon: function() {
+            return jdenticon.toSVG(this.inputText, 200);
+        },
+        reverse: function() {
+            this.inputText.split("").reverse().join("");
+        }
+    },
     methods: {
         onInput: function(event) {
             console.log(event.target.value);
+            this.inputText = event.target.value;
         },
-
-    }
+    },
+    template: 
+    `
+        <div>
+            <h3>My Identicon Generator</h3>
+            <div>Input: <input v-on:input="onInput"/></div>
+            <div>Output: 
+                <div v-html="indenticon"></div>
+                {{inputText}}
+                {{reverse}}
+            </div>
+        </div>
+    `
 })
