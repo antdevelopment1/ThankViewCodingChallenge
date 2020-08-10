b8f9e9eb1256b7abddd087663c2ed6423d54c7a1
April Copes <antdevelopment1@gmail.com>
2020-08-10 19:10:36 -0400

    Adds variations for mounting to vue instance

--- video-browser/src/main.js
+++ video-browser/src/main.js
@@ -1,8 +1,31 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+// new Vue({
+//     el: '#app',
+//     render: function(createElement) {
+//         return createElement(App);
+//     }
+// })
 
+// Equivalent
+// new Vue({
+//     el: '#app',
+//     render: function(h) {
+//         return h(App);
+//     }
+// })
+
+// Equivalent and where to mount vue instance
+// new Vue({
+//     el: '#app',
+//     render: h => h(App)
+// })
+
+// Alternate way to el for element to mount to screen
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: h => h(App)
+}).$mount('#app');
+
+
+
